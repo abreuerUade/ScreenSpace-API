962982c75cbaa72fb5eb799832137a55070ee34b
Andres Breuer <breuer.andres@gmail.com>
2023-06-09 13:49:10 -0300

    cinema update fix

--- src/controllers/handlerFactory.js
+++ src/controllers/handlerFactory.js
@@ -18,7 +18,7 @@ exports.deleteOne = Model =>
 
 exports.updateOne = Model =>
     catchAsync(async (req, res, next) => {
-        if (req.file) req.body.photo = `/images/cinemas/req.file.filename`;
+        if (req.file) req.body.photo = `/images/cinemas/${req.file.filename}`;
         const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true,
