c891339c705857758088c60c15a83f64d9d2e3b5
abreuerUade <102830076+abreuerUade@users.noreply.github.com>
2023-07-02 17:38:52 -0300

    Delete fechas.js

--- fechas.js
+++
@@ -1,7 +0,0 @@
-const duration = 120;
-const cleaningTime = 30;
-const startTime = new Date('2023-03-21 12:00:00Z');
-const finishTime = new Date(startTime).setMinutes(
-    startTime.getMinutes() + duration + cleaningTime
-);
-console.log(finishTime.toLocaleString());
